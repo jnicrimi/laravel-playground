8c1f63a1ad4a4140ab1543764c1cbbe31e4d5384
Junichiro Imai <jnicrimi@gmail.com>
2024-07-06 18:24:21 +0900

    feat: artisan command, clear logs

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('logs:clear', function() {
+    $files = glob(storage_path('logs/*.log'));
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+    $this->comment('Logs have been cleared!');
+})->describe('Clear log files');
