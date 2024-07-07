ee4d0d394fe161af008f1eab31ccfea45018edd8
Junichiro Imai <jnicrimi@gmail.com>
2024-07-07 22:17:18 +0900

    fix: static analysis

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -4,15 +4,17 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
+    /* @phpstan-ignore-next-line */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('logs:clear', function() {
+Artisan::command('logs:clear', function () {
     $files = glob(storage_path('logs/*.log'));
     foreach ($files as $file) {
         if (is_file($file)) {
             unlink($file);
         }
     }
+    /* @phpstan-ignore-next-line */
     $this->comment('Logs have been cleared!');
 })->describe('Clear log files');
