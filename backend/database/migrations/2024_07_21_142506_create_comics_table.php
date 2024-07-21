bb4778108a30f2e1220296c309663d81197bea49
Junichiro Imai <jnicrimi@gmail.com>
2024-07-21 14:35:41 +0900

    fix: migration, add length

--- backend/database/migrations/2024_07_21_142506_create_comics_table.php
+++ backend/database/migrations/2024_07_21_142506_create_comics_table.php
@@ -15,7 +15,7 @@ return new class() extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('key')
+            $table->string('key', 255)
                 ->unique();
             $table->text('name');
             $table->enum('status', ['published', 'draft', 'closed'])
