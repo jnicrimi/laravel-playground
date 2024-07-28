816981cf6d3916fffaa073eb8f889445709fefb3
Junichiro Imai <jnicrimi@gmail.com>
2024-07-28 15:49:03 +0900

    test(phpunit): add seed

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * @var bool
+     */
+    protected $seed = true;
 }
