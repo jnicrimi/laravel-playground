efb3764dd87dd927d2f18dda6f9f5f366c8f4238
Junichiro Imai <jnicrimi@gmail.com>
2025-08-24 23:00:17 +0900

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'build',
+        'ci',
+        'revert',
+      ],
+    ],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
+};
